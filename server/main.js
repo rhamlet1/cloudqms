be68cdf85805f6661ad112dda40661bcf833106a
rhamlet1 <rhamlet@hdcl.co.uk>
2018-03-08 22:41:41 +0000

    Added iron:router package and modified code to make functional. Added library and private folders with router configurations and test collection data respectively. Updated .gitignore file.

--- server/main.js
+++ server/main.js
@@ -1,7 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
+Todos = new Mongo.Collection('todos');
+
 Meteor.startup(() => {
   // code to run on server at startup
+  // Insert sample data if the student collection is empty
+  if (Todos.find().count() === 0) {
+    JSON.parse(Assets.getText("todos.json")).todos.forEach(function (doc) {
+      Todos.insert(doc);
+    });
+  }
 
 });
