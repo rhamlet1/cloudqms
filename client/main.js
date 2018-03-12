425f26b727aa6cd35ea40973ca8d1dcacb261dce
Richard Hamlet <richard.hamlet@optimusaberdeen.com>
2018-03-12 10:26:52 +0000

    Corrected errors

--- client/main.js
+++ client/main.js
@@ -3,26 +3,16 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 //import './main.html';
 
-Structures = new Mongo.Collection('structures');
+//Todos = new Mongo.Collection('todos');
 
-Template.structures.helpers({
-  'structure': function(){
-      return Structures.find({}, { sort: { 'namePath.0': 1 } });
-  },
-  'categories': function(){
-      let options = "";
-      const categoriesList = _.uniq(Structures.find({}, { sort: { 'namePath.0': 1 }, fields: { 'namePath': true } }).map((c) => {
-        return c.namePath[0];
-      }), true);
-//      console.log('categoriesList: ' + categoriesList);
-      categoriesList.splice(0,0,' : ');
-      categoriesList.forEach((keyValuePair) => {
-  //      console.log(keyValuePair);
-        const keyValue = keyValuePair.split(':');
-  //      console.log(keyValue);
-  			options = options + "<option value='" + keyValue[1] + "'>" + keyValue[0] + "</option>";
-  		});
-  //    console.log(options);
-  		return options;
-  }
+//  Todos.insert({
+//    name: "Walk the dog",
+//    completed: false,
+//    createdAt: new Date()
+//});
+
+Template.todos.helpers({
+    'todo': function(){
+        return Todos.find();
+    }
 });
