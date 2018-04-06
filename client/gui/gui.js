ba359fdf3f00e8b31c6d7d759d8fdc9b5a57aa6d
Richard Hamlet <richard.hamlet@optimusaberdeen.com>
2018-04-06 17:21:28 +0100

    Attempt to split text within segments. Work ongoing.

--- client/gui/gui.js
+++ client/gui/gui.js
@@ -8,7 +8,7 @@ Template.gui.onRendered (() => {
   const mongoId = new Mongo.ObjectID;
   // Extract the id String from the object to use as the Meteor id in TreeView
   const meteorId = mongoId._str;
-  const segmentsPerLevel = [[{ name: "Optimus QMS", id: meteorId, flag: true }], levelText];
+  const segmentsPerLevel = [[{ name: "Optimus QMS", id: meteorId, flag: true, lines: 1, lineBreaks: [11] }], levelText];
   console.log('segmentsPerLevel typeof: ' + typeof segmentsPerLevel);
   console.log('segmentsPerLevel: ' + segmentsPerLevel);
   draw(segmentsPerLevel);
@@ -158,13 +158,13 @@ Template.gui.events ({
       const mongoId = new Mongo.ObjectID;
       // Extract the id String from the object to use as the Meteor id in TreeView
       const meteorId = mongoId._str;
-      const firstLevel = [{ name: "Optimus QMS", id: meteorId, flag: true }];
+      const firstLevel = [{ name: "Optimus QMS", id: meteorId, flag: true, lines: 1, lineBreaks: [11] }];
 
       if (segmentLevel === 0) {
         levelText.splice(0, 0, firstLevel);
         // add children of the clicked variable in next level down
         children = TreeData.find({ parent: null }).map((item) => {
-          return { name: item.name, id: item._id, parent: item.parent, flag: false};
+          return { name: item.name, id: item._id, parent: item.parent, flag: false, lines: 1, lineBreaks: [item.name.length]};
         });
         selectedSegmentId = id;
         selectedSegmentName = segmentName;
@@ -185,14 +185,14 @@ Template.gui.events ({
 
         for (let q = segmentLevel; q >= 1; q = q - 1) {
           const thisSegment = TreeData.find({ _id: id }).map((item) => {
-            return { name: item.name, id: item._id, parent: item.parent, flag: true };
+            return { name: item.name, id: item._id, parent: item.parent, flag: true, lines: 1, lineBreaks: [item.name.length] };
           });
           thisSegmentId = thisSegment[0].id;
           console.log('thisSegment: ' + thisSegment);
           console.log('thisSegmentId: ' + thisSegmentId);
           // get all the selected segment's siblings
           const siblings = TreeData.find({ parent: thisSegment[0].parent }).map((item) => {
-            return { name: item.name, id: item._id, parent: item.parent, flag: false};
+            return { name: item.name, id: item._id, parent: item.parent, flag: false, lines: 1, lineBreaks: [item.name.length]};
           });
           console.log('siblings: ' + siblings);
           // go through the siblings and change the flag of the selected sibling
@@ -212,7 +212,7 @@ Template.gui.events ({
         levelText.splice(0, 0, firstLevel);
         // add children of the clicked variable in next level down
         children = TreeData.find({ parent: selectedSegmentId }).map((item) => {
-          return { name: item.name, id: item._id, parent: item.parent, flag: false};
+          return { name: item.name, id: item._id, parent: item.parent, flag: false, lines: 1, lineBreaks: [item.name.length] };
         });
         if (children.length > 0) {
           levelText.push(children);
@@ -255,7 +255,10 @@ function draw(segments) {
     const ctry = canvas.height / 2;
 
     for (let i = levels; i >= 1; i = i - 1) {
+
       for (let j = 0; j < segments[i-1].length; j = j + 1) {
+
+        // ******* THIS SECTION RELATES TO DRAWING THE SEGMENTS *******
         // calculate drawing coordinates
         const outerRadius = i * ctry / (1 + levels);
         const innerRadius = (i - 1) * ctry / (1 + levels);
@@ -267,7 +270,7 @@ function draw(segments) {
         let arcRadius = 0;
         let r = outerRadius;
 
-        if ( i !== 1) {
+        if ( i !== 1) {  // calculate the centre of each segment to apply colour gradient
           arcRadius = (outerRadius - innerRadius) / 2 + innerRadius;
           let arcLength = arcRadius * angle;
           r = arcLength / 2;
@@ -312,46 +315,89 @@ function draw(segments) {
         ctx.stroke();
         ctx.fill();
 
-        // add text to the segment
-        // first size the text font to fit its segment
+
+        // ******* THIS SECTION RELATES TO ADDING THE TEXT *******
+        // determine the number of lines of text in each segment and the font size for the level
+        let lines = segments[i-1][j].lines;  // no of lines in segments
+        let lineBreaks = segments[i-1][j].lineBreaks;  // line break points measured from start of text
+
+        // set font size constraints and default font context
+        const minFontHeight = 10;
+        const maxFontHeight = 30;
         ctx.textAlign = "center";
+        ctx.textBaseline= "middle";
         ctx.fillStyle = fillCol[0];  // text colour black
         ctx.font = "bold 30px Serif";  // set an arbitrary font size;
 
-        let text = " " + segments[i - 1][j].name + " ";  // retrieve the segment text and add padding
-        let metrics = ctx.measureText(text);  // get information about text
+        // set variables initial values
         let fontHeight = 30;
-        let textRadius = (outerRadius - innerRadius) / 2 - fontHeight / 2 + innerRadius;
-        arcLength = textRadius * angle;
-        let textScale = metrics.width * 1.2 / arcLength;  // proportion font size
-        // iterate font size scaling until within accepted limit
-        while (textScale > 1) {
-          fontHeight = Math.floor(30 / textScale)
-          const scaledFont = 'bold ' + fontHeight.toString() + 'px Serif';
-          ctx.font = scaledFont;
-          metrics = ctx.measureText(text);  // get information about text
-          textRadius = (outerRadius - innerRadius) / 2 - fontHeight / 2 + innerRadius;
+
+        // add text to the segment
+        // first size the text font to fit its segment
+        for (let k = 0; k < lines; k = k + 1) {
+          // split text into number of required lines
+          text = " " + segments[i - 1][j].name + " ";  // retrieve the segment text and add padding
+          let metrics = ctx.measureText(text);  // get information about text
+          let textRadius = (outerRadius - innerRadius) / lines * ((k + 1) - (lines + 1) / 2) + innerRadius;
           arcLength = textRadius * angle;
-          textScale = metrics.width / arcLength / 1.2;  // proportion font size
-        }
+
+          if (textRadius > arcLength) {  // need to split text
+            textLength = text.length;
+            lines = lines + 1;
+            k = -1;  // force the loop to start again
+            let breakPoints = Math.floor((textLength - 1) / (lines - 1));
+            for (let l = 0; l < lines - 1; l = l + 1) {  // each line in segment
+              lineBreaks[l] = breakPoints * (l + 1) + 1;  // default break point if no 'special' character found
+              for (let m = 0; m < breakPoints + 1; m = m + 1) {  // each letter in text
+                if (text.charAt(breakPoints + m) === ' ' || text.charAt(breakPoints + m) === '/' || text.charAt(breakPoints + m) === '-') {
+                  lineBreaks[l] = breakPoints + m;
+                  break;
+                } else if (text.charAt(breakPoints - m) === ' ' || text.charAt(breakPoints - m) === '/' || text.charAt(breakPoints - m) === '-') {
+                  lineBreaks[l] = breakPoints - m;
+                  break;
+                }
+              }
+              breakPoints = Math.floor((textLength - lineBreaks[l] - 1) / (lines - l - 1)); // resets the breakPoint based on previous breakPoint
+            }
+          }
+
+          console.log('lineBreaks: ' + lineBreaks.length);
+          for (let s = 0; s < lineBreaks.length; s += 1) {
+            console.log('lineBreaks[' + s + ']: ' + lineBreaks[s]);
+          }
+
+/*
+
+          let textScale = metrics.width * 1.2 / arcLength;  // proportion font size
+          // iterate font size scaling until within accepted limit
+          while (textScale > 1) {
+            fontHeight = Math.floor(30 / textScale)
+            const scaledFont = 'bold ' + fontHeight.toString() + 'px Serif';
+            ctx.font = scaledFont;
+            metrics = ctx.measureText(text);  // get information about text
+            textRadius = (outerRadius - innerRadius) / 2 - fontHeight / 2 + innerRadius;
+            arcLength = textRadius * angle;
+            textScale = metrics.width / arcLength / 1.2;  // proportion font size
+          }
+*/
 
         // draw the text
-        let numDegreesPerLetter = angle / text.length;
-        ctx.save();
-        ctx.translate(ctrx, ctry);  // centre of canvas
-        ctx.rotate(startAngle + numDegreesPerLetter / 2);
-
-        for (var k=0; k<text.length; k++){
+          let numDegreesPerLetter = angle / text.length;
           ctx.save();
-          ctx.translate(textRadius, 0);
-          ctx.rotate(Math.PI / 2);
-          ctx.translate(0, -fontHeight / 2);
-          ctx.fillText(text[k], 0, 0);
-          ctx.restore();
-          ctx.rotate(numDegreesPerLetter);
-        }
-        ctx.restore();
+          ctx.translate(ctrx, ctry);  // centre of canvas
+          ctx.rotate(startAngle + numDegreesPerLetter / 2);
 
+          for (var k=0; k<text.length; k++){
+            ctx.save();
+            ctx.translate(textRadius, 0);
+            ctx.rotate(Math.PI / 2);
+            ctx.translate(0, -fontHeight / 2);
+            ctx.fillText(text[k], 0, 0);
+            ctx.restore();
+            ctx.rotate(numDegreesPerLetter);
+          }
+          ctx.restore();
+        }
       }
     }
   }
